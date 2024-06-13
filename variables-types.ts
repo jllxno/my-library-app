86c13da33171e1adb1eb6c8a4a7f77d03dcefd77
jllxno <juanllxno201@gmail.com>
2024-06-13 06:53:32 -0500

    tipado primitivo

---
+++ variables-types.ts
@@ -0,0 +1,29 @@
+
+let bookTitle: string = "TypeScript Basics";
+
+
+const libraryName: string = "My Library";
+
+
+let numberOfBooks: number = 10;
+
+
+let isLibraryOpen: boolean = true;
+
+
+let titulo: string = "El nombre del viento";
+let autor: string = "Patrick Rothfuss";
+let genero: string = "Fantasía";
+let idioma: string = "Español";
+let precio: number = 20.50; 
+let formato: string = "Tapa blanda";
+let isbn: string = "9788401352836";
+let descripcion: string = "Una historia épica que sigue los pasos de Kvothe, un joven músico y aventurero con un don para la magia.";
+let estado: string = "Nuevo";
+let ubicacion: string = "Sección de Fantasía, estante 3A";
+let fecha_publicacion: Date = new Date(2007, 2, 27);
+let editorial: string = "Ediciones B";
+let paginas: number = 816;
+let dimensiones: [number, number, number] = [15, 4, 23]; // Ancho x Profundidad x Altura en centímetros
+let peso: number = 700;
+let disponible: boolean = true; 
